d9c854cb5006bbbc4916b8d2ade791218583fd85
jminardi <jminardi@enthought.com>
2013-07-13 01:04:16 -0500

    rebuild

--- .source/push-to-gh.sh
+++ .source/push-to-gh.sh
@@ -1,22 +1,9 @@
 #! /bin/bash
 
-make html
+#make html
 cd ..
 ls | grep -v CNAME | xargs rm -rf
 mv .source/output/* .
 git add .
 git commit -m 'rebuild'
 git push origin master
-
-
-#rm -rf .old-gh-pages
-#mv gh-pages .old-gh-pages
-#cp -r output/ gh-pages/
-#cp -r .old-gh-pages/.git gh-pages/.git
-#cp  .old-gh-pages/CNAME gh-pages/CNAME
-#cd gh-pages
-#git add .
-#git commit -m
-#git push origin master
-#cd ..
-#rm -rf .old-gh-pages
